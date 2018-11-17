3f771bcc0c34fb6f7106c527ce8aa0eced920f6b
Erik Pohle <erik.pohle@tum.de>
2018-11-17 22:00:54 +0100

    Finished frontend for open to carpool

--- SixtCarpool/app/src/main/java/de/hackatum2018/sixtcarpool/activities/OpenCarpoolFragment.kt
+++ SixtCarpool/app/src/main/java/de/hackatum2018/sixtcarpool/activities/OpenCarpoolFragment.kt
@@ -1,6 +1,8 @@
 package de.hackatum2018.sixtcarpool.activities
 
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -9,13 +11,17 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import de.hackatum2018.sixtcarpool.R
 import de.hackatum2018.sixtcarpool.Repository
 import de.hackatum2018.sixtcarpool.database.AppDatabase
 import de.hackatum2018.sixtcarpool.subscribeInLifecycle
 import de.hackatum2018.sixtcarpool.viewmodels.OpenCarpoolViewModel
 import kotlinx.android.synthetic.main.fragment_open_carpool.view.*
+import java.text.DateFormat
+import java.util.*
 
 
 class OpenCarpoolFragment : Fragment() {
@@ -66,15 +72,22 @@ class OpenCarpoolFragment : Fragment() {
         toEdit.addTextChangedListener(TextChangedListener {
             viewmodel.to = it?.toString() ?: ""
         })
-        timeEdit.addTextChangedListener(TextChangedListener {
-            viewmodel.startDateTime = it?.toString()?.toLong() ?: 0
-        })
-        maxPassengerSpinner.setOnItemClickListener { _, _, position, _ ->
-            viewmodel.maxPassengers = position
+        maxPassengerSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                viewmodel.maxPassengers = position + 1
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
         }
         priceEdit.addTextChangedListener(TextChangedListener {
             viewmodel.price = it?.toString()?.toInt() ?: 0
         })
+
+        timeEdit.setOnClickListener {
+            //open time and date picker dialog
+            datePicker(timeEdit)
+        }
 
         //enable/disable button
         val button = rootView.open_carpool_btn
@@ -86,6 +99,50 @@ class OpenCarpoolFragment : Fragment() {
         }
 
         return rootView
+    }
+
+    private fun datePicker(textView: TextView) {
+
+
+        // Get Current Date
+
+        val c = Calendar.getInstance()
+        if (viewmodel.startDateTime > System.currentTimeMillis() / 1000) {
+            c.timeInMillis = viewmodel.startDateTime * 1000
+        }
+        val y = c.get(Calendar.YEAR)
+        val m = c.get(Calendar.MONTH)
+        val d = c.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            context,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                timePicker(textView, y, m, d)
+            }, y, m, d)
+        datePickerDialog.show()
+        datePickerDialog.datePicker.minDate = System.currentTimeMillis() -1000
+    }
+
+    private fun timePicker(textView: TextView, year: Int, month: Int, day: Int) {
+        // Get Current Time
+        val c = Calendar.getInstance()
+        val h = c.get(Calendar.HOUR_OF_DAY)
+        val m = c.get(Calendar.MINUTE)
+
+        // Launch Time Picker Dialog
+        val timePickerDialog = TimePickerDialog(
+            context,
+            TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                c.set(year, month, day, hourOfDay, minute)
+                val unixTime = c.timeInMillis / 1000
+                viewmodel.startDateTime = unixTime
+                val formatter = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT)
+                formatter.calendar = c
+                textView.text = formatter.format(c.time)
+
+            }, h, m, true
+        )
+        timePickerDialog.show()
     }
 
 
--- SixtCarpool/app/src/main/java/de/hackatum2018/sixtcarpool/database/entities/CarRental.kt
+++ SixtCarpool/app/src/main/java/de/hackatum2018/sixtcarpool/database/entities/CarRental.kt
@@ -6,18 +6,19 @@ import android.arch.persistence.room.*
  * Created by Aleksandr Kim on 17 Nov, 2018 12:27 PM for SixtCarpool
  */
 @Entity(tableName = CarRental.TABLE_NAME)
-data class CarRental(@PrimaryKey val id: Int,
-                     val name: String,
-                     @ColumnInfo(name = MAX_PASSENGERS_COLUMN) val maxPassengers: Int,
-                     val imageUrl: String,
-                     val pickupStationId: Long,
-                     val pickupStationName: String,
-                     val pickupTime: Long,
-                     val returnStationId: Long,
-                     val returnStationName: String,
-                     val returnTime: Long,
-                     val price: Long,
-                     @Embedded var carpoolOffer: CarpoolOffer?
+data class CarRental(
+    @PrimaryKey val id: Int,
+    val name: String,
+    @ColumnInfo(name = MAX_PASSENGERS_COLUMN) val maxPassengers: Int,
+    val imageUrl: String,
+    val pickupStationId: Long,
+    val pickupStationName: String,
+    val pickupTime: Long,
+    val returnStationId: Long,
+    val returnStationName: String,
+    val returnTime: Long,
+    val price: Long,
+    @Embedded var carpoolOffer: CarpoolOffer?
 ) {
 
     companion object {
@@ -25,8 +26,20 @@ data class CarRental(@PrimaryKey val id: Int,
         const val MAX_PASSENGERS_COLUMN = "maxPassengers_car"
 
         fun dummy(rentalId: Int): CarRental {
-            return CarRental(rentalId, "my rented car", 4, "", 1,
-                "center", 1234, 2, "campus", 2345, 100, null)
+            return CarRental(
+                rentalId,
+                "Chevo",
+                4,
+                "http://www.sixtblog.com/wp-content/uploads/2015/01/bmw_x3-sixt-rental-car_d31f5b.png",
+                1,
+                "center",
+                1234,
+                2,
+                "campus",
+                2345,
+                100,
+                null
+            )
         }
 
         fun dummyWithOffer(rentalId: Int, carpoolOfferId: Int): CarRental {
