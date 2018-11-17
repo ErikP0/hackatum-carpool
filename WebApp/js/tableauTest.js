1e08d6c6559257d57458a3bc20feda35b32788cc
Fuyuan Li <ga26piq@mytum.de>
2018-11-17 16:27:25 +0100

    Shorten function in js script

--- WebApp/js/tableauTest.js
+++ WebApp/js/tableauTest.js
@@ -1,107 +1,51 @@
-/*<script type="text/javascript">
-        var parentElement = document.getElementsByClassName('container-right')[0];
-        var divElement = document.getElementById('viz1542452204883');
-        var vizElement = divElement.getElementsByTagName('object')[0];
-        vizElement.style.width = '1000px';
-        vizElement.style.height = '827px';
-        /*vizElement.style.width = 0.8 * parentElement.offsetWidth + "px";
-        console.log(0.8 * parentElement.offsetWidth + "px");
-        vizElement.style.height = 0.8 * parentElement.offsetHeight + "px";
-        console.log(parentElement.offsetHeight);
-        divElement.style.marginLeft = 0.1 * parentElement.offsetWidth + "px";
-        divElement.style.marginTop = 0.1 * parentElement.offsetHeight + "px";
-        var scriptElement = document.createElement('script');
-        scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-        vizElement.parentNode.insertBefore(scriptElement, vizElement);
-    </script>
+var driver = {
+    complicate : false,
+    path : "shared&#47;33SF8BXFP",
+    static_image : "https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;33&#47;33SF8BXFP&#47;1.png",
+    width: '1000px',
+    height: '827px'
+};
 
-<div class='tableauPlaceholder' id='viz1542452204883'>
-<noscript>
-    <a href='#'>
-        <img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;33&#47;33SF8BXFP&#47;1_rss.png'
-            style='border: none' />
-    </a>
-</noscript>
+var passenger = {
+    complicate : true,
+    name : "ALTNATION&#47;AltNation",
+    static_image : "https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;AL&#47;ALTNATION&#47;AltNation&#47;1.png",
+    width: '1450px',
+    height: '2527px'
+};
 
-<object class='tableauViz' style='display:none;'>
-    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-    <param name='embed_code_version' value='3' />
-    <param name='path' value='shared&#47;33SF8BXFP' />
-    <param name='toolbar' value='yes' />
-    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;33&#47;33SF8BXFP&#47;1.png' />
-    <param name='animate_transition' value='yes' />
-    <param name='display_static_image' value='yes' />
-    <param name='display_spinner' value='yes' />
-    <param name='display_overlay' value='yes' />
-    <param name='display_count' value='yes' />
-</object>
-</div>*/
+var stuff = {
+    complicate : true,
+    name : "StarMap_1&#47;Dashboard3",
+    static_image : "https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;St&#47;StarMap_1&#47;Dashboard3&#47;1.png",
+    width: '1400px',
+    height: '827px'
+};
 
-function changeToDriverDashboard() {
+function changeDashboard(member){
     var divElement = document.getElementById('viz1542464115990');
-    divElement.innerHTML = '<object class="tableauViz" style="display:none;">' +
+    var str = '<object class="tableauViz" style="display:none;">' +
         '<param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />' +
-        '<param name="embed_code_version" value="3" />' +
-        '<param name="path" value="shared&#47;33SF8BXFP" />' +
-        '<param name="toolbar" value="yes" />' +
-        '<param name="static_image" value="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;33&#47;33SF8BXFP&#47;1.png" />' +
+        '<param name="embed_code_version" value="3" />';
+    if(member.complicate){
+        str += '<param name="site_root" value="" />' +
+        '<param name="name" value="' + member.name + '" />' +
+        '<param name="tabs" value="no" />';
+    }else{
+        str += '<param name="path" value="' + member.path + '" />'
+    }     
+        str += '<param name="toolbar" value="yes" />' +
+        '<param name="static_image" value="' + member.static_image + '" />' +
         '<param name="animate_transition" value="yes" />' +
         '<param name="display_static_image" value="yes" />' +
         '<param name="display_spinner" value="yes" />' +
         '<param name="display_overlay" value="yes" />' +
         '<param name="display_count" value="yes" />' +
         '</object>';
+    divElement.innerHTML = str;
     var vizElement = divElement.getElementsByTagName('object')[0];
-    vizElement.style.width = '1450px';
-    vizElement.style.height = '2527px';
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
-}
-
-function changeToPassengerDashboard() {
-    var divElement = document.getElementById('viz1542464115990');
-    divElement.innerHTML = '<object class="tableauViz" style="display:none;">' +
-        '<param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />' +
-        '<param name="embed_code_version" value="3" />' +
-        '<param name="site_root" value="" />' +
-        '<param name="name" value="ALTNATION&#47;AltNation" />' +
-        '<param name="tabs" value="no" />' +
-        '<param name="toolbar" value="yes" />' +
-        '<param name="static_image" value="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;AL&#47;ALTNATION&#47;AltNation&#47;1.png" />' +
-        '<param name="animate_transition" value="yes" />' +
-        '<param name="display_static_image" value="yes" />' +
-        '<param name="display_spinner" value="yes" />' +
-        '<param name="display_overlay" value="yes" />' +
-        '<param name="display_count" value="yes" />' +
-        '</object>';
-    var vizElement = divElement.getElementsByTagName('object')[0];
-    vizElement.style.width = '1450px';
-    vizElement.style.height = '2527px';
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
-}
-
-function changeToStuffDashboard() {
-    var divElement = document.getElementById('viz1542464115990');
-    divElement.innerHTML = '<object class="tableauViz" style="display:none;">' +
-        '<param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />' +
-        '<param name="embed_code_version" value="3" />' +
-        '<param name="site_root" value="" />' +
-        '<param name="name" value="StarMap_1&#47;Dashboard3" />' +
-        '<param name="tabs" value="no" />' +
-        '<param name="toolbar" value="yes" />' +
-        '<param name="static_image" value="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;St&#47;StarMap_1&#47;Dashboard3&#47;1.png" />' +
-        '<param name="animate_transition" value="yes" />' +
-        '<param name="display_static_image" value="yes" />' +
-        '<param name="display_spinner" value="yes" />' +
-        '<param name="display_overlay" value="yes" />' +
-        '<param name="display_count" value="yes" />' +
-        '</object>';
-    var vizElement = divElement.getElementsByTagName('object')[0];
-    vizElement.style.width = '1400px';
-    vizElement.style.height = '827px';
+    vizElement.style.width = member.width;
+    vizElement.style.height = member.height;
     var scriptElement = document.createElement('script');
     scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
     vizElement.parentNode.insertBefore(scriptElement, vizElement);
